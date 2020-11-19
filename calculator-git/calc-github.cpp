fa046a25d9af49d133b22dd6641e9403ee7cb807
Ghada <ghadaadelnady@yahoo.com>
2020-11-19 09:36:41 +0200

    added subtraction

--- calculator-git/calc-github.cpp
+++ calculator-git/calc-github.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 void main()
 {
-	int x, y, z;
+	int x=0, y=5, z;
 	z = x + y;
+	x = z - 2;
 	cout << "addition of x + y = " << z << endl;
+	cout << "subtraction of 2 from z = " << x << endl;
 }
